f588a528bfe7b336a470665106645c288760c687
Shahab999 <121120439+Shahab999@users.noreply.github.com>
2023-01-24 01:10:23 +0330

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,19 +13,19 @@
             <div class="row justify-content-center">
                 <div class="col-md-4">
                     <div class="card">
-                        <h3 class="card-header text-center">Login</h3>
+                        <h3 class="card-header text-center">ورود</h3>
                         <div class="card-body">
                             <form method="POST" action="{{ route('login.custom') }}">
                                 @csrf
                                 <div class="form-group mb-3">
-                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
+                                    <input type="text" placeholder="ایمیل" id="email" class="form-control" name="email" required
                                         autofocus>
                                     @if ($errors->has('email'))
                                     <span class="text-danger">{{ $errors->first('email') }}</span>
                                     @endif
                                 </div>
                                 <div class="form-group mb-3">
-                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
+                                    <input type="password" placeholder="گذرواژه" id="password" class="form-control" name="password" required>
                                     @if ($errors->has('password'))
                                     <span class="text-danger">{{ $errors->first('password') }}</span>
                                     @endif
@@ -38,8 +38,11 @@
                                     </div>
                                 </div>
                                 <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Signin</button>
+                                    <button type="submit" class="btn btn-dark btn-block">Sign in</button>
                                 </div>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register-user') }}">Sign up</a>
+                                </li>
                             </form>
                         </div>
                     </div>
